0ec50bf1a1288f1b41f84b693c21694ecadb2c78
Davis Haupt <davis.haupt@gmail.com>
2021-04-11 15:34:54 -0400

    changes

--- common.py
+++ common.py
@@ -7,14 +7,3 @@ def to_string(d):
 
 def from_string(s):
     return datetime.strptime(s, fmt).time()
-
-def set_data(fname, s):
-    with open(fname, "w") as f:
-        f.write(s)
-
-def get_data(fname, parse=lambda x: x, default=None):
-    try:
-        with open(fname, "r") as f:
-            return parse(f.read())
-    except FileNotFoundError, Exception:
-        return default
--- tick.py
+++ tick.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
+import json
+
 import board
 from adafruit_ht16k33.segments import Seg7x4
+
+from common import from_string
+
+SLEEP_SETTINGS = "settings/sleep.json"
+
+def get_time_string(d):
+    time_string = d.strftime("%I:%M")
+    if time_string.startswith("0"):
+        time_string = " " + time_string[1:]
+    return time_string
 
 def main():
     i2c = board.I2C()
     display = Seg7x4(i2c)
 
     display.brightness = 0.5
+
     now = datetime.now()
-    current_time = now.strftime("%I:%M")
-    if current_time.startswith("0"):
-        current_time = " " + current_time[1:]
-    display.print(current_time)
+
+    with open(SLEEP_SETTINGS, "r") as f:
+        sleep_settings = json.load(f)
+
+    nowt = now.time()
+
+    if from_string(json["begin"]) < nowt or nowt < from_string(json["end"]):
+        display.fill(0)
+    else:
+        display.print(current_time)
 
 if __name__ == "__main__":
     main()
